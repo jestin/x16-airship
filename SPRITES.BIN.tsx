91f7d5c350973eea92dd642baf887279d09a115c
Jestin Stoffel <jestin.stoffel@gmail.com>
2022-03-01 14:21:03 -0600

    Add forward walk cycles and animations in tiled

--- SPRITES.BIN.tsx
+++ SPRITES.BIN.tsx
@@ -1,2 +1,36 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.5" tiledversion="1.8.0" name="/home/jestin/Projects/x16-airship/SPRITES.BIN" tilewidth="16" tileheight="16" tilecount="64" columns="8"><image source="/home/jestin/Projects/x16-airship/SPRITES.BIN.bmp" trans="000000" width="128" height="128"/></tileset>
+<tileset version="1.8" tiledversion="1.8.2" name="/home/jestin/Projects/x16-airship/SPRITES.BIN" tilewidth="16" tileheight="16" tilecount="64" columns="8">
+ <image source="SPRITES.BIN.bmp" trans="000000" width="128" height="128"/>
+ <tile id="0">
+  <animation>
+   <frame tileid="0" duration="133"/>
+   <frame tileid="8" duration="133"/>
+   <frame tileid="0" duration="133"/>
+   <frame tileid="16" duration="133"/>
+  </animation>
+ </tile>
+ <tile id="1">
+  <animation>
+   <frame tileid="1" duration="133"/>
+   <frame tileid="9" duration="133"/>
+   <frame tileid="1" duration="133"/>
+   <frame tileid="17" duration="133"/>
+  </animation>
+ </tile>
+ <tile id="2">
+  <animation>
+   <frame tileid="2" duration="133"/>
+   <frame tileid="10" duration="133"/>
+   <frame tileid="2" duration="133"/>
+   <frame tileid="18" duration="133"/>
+  </animation>
+ </tile>
+ <tile id="3">
+  <animation>
+   <frame tileid="3" duration="133"/>
+   <frame tileid="11" duration="133"/>
+   <frame tileid="3" duration="133"/>
+   <frame tileid="19" duration="133"/>
+  </animation>
+ </tile>
+</tileset>
