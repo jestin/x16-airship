779afb6b2ac8e47a76c3ed3aa549ce17424fe303
Jestin Stoffel <jestin.stoffel@gmail.com>
2022-03-12 21:06:29 -0600

    Create multi-directional animations for Rorie

---
+++ AURORA.BIN.tsx
@@ -0,0 +1,28 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.2" name="/home/jestin/Projects/x16-airship/AURORA.BIN" tilewidth="16" tileheight="16" tilecount="12" columns="12">
+ <image source="AURORA.BIN.bmp" trans="000000" width="192" height="16"/>
+ <tile id="3">
+  <animation>
+   <frame tileid="3" duration="133"/>
+   <frame tileid="4" duration="133"/>
+   <frame tileid="3" duration="133"/>
+   <frame tileid="5" duration="133"/>
+  </animation>
+ </tile>
+ <tile id="6">
+  <animation>
+   <frame tileid="6" duration="133"/>
+   <frame tileid="7" duration="133"/>
+   <frame tileid="6" duration="133"/>
+   <frame tileid="8" duration="133"/>
+  </animation>
+ </tile>
+ <tile id="9">
+  <animation>
+   <frame tileid="9" duration="133"/>
+   <frame tileid="10" duration="133"/>
+   <frame tileid="9" duration="133"/>
+   <frame tileid="11" duration="133"/>
+  </animation>
+ </tile>
+</tileset>
