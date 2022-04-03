0813da04042009358e99c4782eccf30883133c85
Jestin Stoffel <jestin.stoffel@gmail.com>
2022-04-03 02:33:49 -0500

    Draw animated water tiles

--- pixryn_isles/PITILES.BIN.tsx
+++ pixryn_isles/PITILES.BIN.tsx
@@ -1,6 +1,14 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.8" tiledversion="1.8.2" name="/home/jestin/Projects/x16-airship/PITILES.BIN" tilewidth="16" tileheight="16" tilecount="256" columns="16">
  <image source="PITILES.BIN.bmp" trans="000000" width="256" height="256"/>
+ <tile id="1">
+  <animation>
+   <frame tileid="1" duration="250"/>
+   <frame tileid="2" duration="250"/>
+   <frame tileid="3" duration="250"/>
+   <frame tileid="4" duration="250"/>
+  </animation>
+ </tile>
  <tile id="2" probability="0.05"/>
  <tile id="3" probability="0.05"/>
  <tile id="4" probability="0.05"/>
