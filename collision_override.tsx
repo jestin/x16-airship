2ee4ec5b9027441be2433da0b437d064b5dcdd7a
Jestin Stoffel <jestin.stoffel@gmail.com>
2022-04-11 10:02:46 -0500

    Add collision override tileset and rules

---
+++ collision_override.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.2" name="collision_override" tilewidth="16" tileheight="16" tilecount="64" columns="8">
+ <image source="collision_override_tiles.bmp" trans="000000" width="128" height="128"/>
+</tileset>
