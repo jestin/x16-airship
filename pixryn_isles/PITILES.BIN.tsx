315dd5f0f038bd2c69bbca71d2f5a339797cc988
Jestin Stoffel <jestin.stoffel@gmail.com>
2022-04-19 10:58:11 -0500

    Put correct path in TSX file

--- pixryn_isles/PITILES.BIN.tsx
+++ pixryn_isles/PITILES.BIN.tsx
@@ -1,5 +1,5 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.8" tiledversion="1.8.2" name="/home/jestin/Projects/x16-airship/PITILES.BIN" tilewidth="16" tileheight="16" tilecount="256" columns="16">
+<tileset version="1.8" tiledversion="1.8.2" name="/home/jestin/Projects/x16-airship/pixryn_isles/PITILES.BIN" tilewidth="16" tileheight="16" tilecount="256" columns="16">
  <image source="PITILES.BIN.bmp" trans="000000" width="256" height="256"/>
  <tile id="1">
   <animation>
