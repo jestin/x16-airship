1db13740eec214bc1c185fb2c037ccc57eeb613e
Jestin Stoffel <jestin.stoffel@gmail.com>
2022-05-25 12:52:31 -0500

    Add comment to explain message file

--- pixryn_isles/messages.asm
+++ pixryn_isles/messages.asm
@@ -1,3 +1,7 @@
+; This file is built by an assembler, but not actually built into the
+; application.  Instead, it will be loaded up as data by the main program.
+
+; These are dummy values, only here to keep the assembler from complaining
 .org $A000
 .segment "STARTUP"
 .segment "INIT"
