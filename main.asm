a97ca3222ba309698c278a96bd7063d2ef74d7c6
Jestin Stoffel <jestin.stoffel@gmail.com>
2022-11-06 00:06:59 -0500

    Add a sprite for Will

--- main.asm
+++ main.asm
@@ -82,8 +82,8 @@ main:
 	jsr LOAD
 
 	; TODO: Move this to a player selection screen
-	LoadW player_file, aurorafile
-	LoadW player_file_size, end_aurorafile - aurorafile
+	LoadW player_file, willfile
+	LoadW player_file_size, end_willfile - willfile
 
 	LoadWBE player_collision_tile+00, %0000000000000000
 	LoadWBE player_collision_tile+02, %0000000000000000
